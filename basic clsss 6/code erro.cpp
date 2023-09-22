27602f46be8416d5325fc3ccbb47d2fa75493232
Israr Ullah <126203984+Israrshahwani090@users.noreply.github.com>
2023-09-22 22:35:29 +0500

    Errors in code //home assignment.

---
+++ basic clsss 6/code erro.cpp
@@ -0,0 +1,20 @@
+/*
+Home Assignment//22/09/2023
+
+Q8.8: question identify and correct error
+*/
+
+
+#include <iostream>
+using namespace std;
+
+// corect code side___
+#include <iostream>
+int main()           
+{
+int a,b,s;  
+cout<<"Enter numbers: ";
+cin>>a>>b;
+s = a + b;
+cout<<"\n"<<s;
+}
